3a1a9da0daaacffbc32e7f1fde236ba11fc9a46e
weaponsforge <forgeweapons@gmail.com>
2020-04-05 18:19:42 +0800

    Used dotenv. Updated README.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const app = express()
+const serviceUrl = process.env.NODE_ENV === 'production' ?
+  process.env.PRERENDER_SERVICE_URL :  process.env.PRERENDER_SERVICE_URL_DEV
 
 app.set('trust proxy', 1)
-app.use(require('prerender-node').set('prerenderServiceUrl', 'https://prerenderservice.herokuapp.com/render'))
+app.use(require('prerender-node').set('prerenderServiceUrl', serviceUrl))
 app.use(express.static('public'))
 
 app.listen(PORT, () => {
