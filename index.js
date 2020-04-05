755ecf81ffae0e457a85195bd30e921b95b18809
weaponsforge <forgeweapons@gmail.com>
2020-04-05 16:06:56 +0800

    Used prerender middleware.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.set('trust proxy', 1)
+app.use(require('prerender-node').set('prerenderServiceUrl', 'https://prerenderservice.herokuapp.com/render'))
 app.use(express.static('public'))
 
 app.listen(PORT, () => {
